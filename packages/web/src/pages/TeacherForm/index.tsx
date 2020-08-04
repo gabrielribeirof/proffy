7c54991fb6a1e98c7222efafbbaf1cdf9ffa374e
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-04 02:24:40 -0300

    chore(web/TeacherForm): create teacher form page

---
+++ packages/web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+const TeacherForm: React.FC = () => (
+  <>
+    <PageHeader title="How amazing you want to teach" />
+  </>
+);
+
+export default TeacherForm;
