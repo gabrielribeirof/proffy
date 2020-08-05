8a4c122f9c27852b1aadff14c2591c381d2a4efd
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-05 13:37:11 -0300

    feat(web/Select): create select component

---
+++ packages/web/src/components/Select/index.tsx
@@ -0,0 +1,28 @@
+import React, { SelectHTMLAttributes } from 'react';
+
+import { Container } from './styles';
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = ({
+  name, label, options, ...rest
+}) => (
+  <Container>
+    <label htmlFor={name}>{label}</label>
+    <select id={name} {...rest}>
+      <option value="" disable selected hidden>Select a option</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>{option.label}</option>
+      ))}
+    </select>
+  </Container>
+);
+
+export default Select;
---
+++ packages/web/src/components/Select/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 14px;
+  position: relative;
+
+  :first-child {
+    margin-top: 0;
+  }
+
+  @media (min-width: 700px) {
+    :first-child {
+      margin-top: 0;
+    }
+  }
+
+  label {
+    font-size: 14px;
+  }
+
+  select {
+    width: 100%;
+    height: 56px;
+    min-height: 16px;
+    padding: 12px 16px;
+    border: 1px solid var(--color-line-in-white);
+    border-radius: 8px;
+    resize: vertical;
+    outline: 0;
+
+    background: var(--color-input-background);
+    font: 16px Archivo;
+  }
+
+  :focus-within::after {
+    content: '';
+    width: calc(100% - 32px);
+    height: 2px;
+    position: absolute;
+    left: 16px;
+    right: 16px;
+    bottom: 0;
+
+    background: var(--color-primary-light);
+  }
+`;
