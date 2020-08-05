8f33f11218f97e940d0e1ed0105c6707a26ea7cb
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-05 13:38:22 -0300

    feat(web/TeacherForm): create initial teacher form

--- packages/web/src/pages/TeacherForm/index.tsx
+++ packages/web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,92 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import { Container, Box } from './styles';
 
 const TeacherForm: React.FC = () => (
-  <>
-    <PageHeader title="How amazing you want to teach" />
-  </>
+  <Container>
+    <PageHeader
+      title="How amazing you want to teach"
+      description="Teste"
+    />
+
+    <main>
+      <Box>
+        <fieldset>
+          <legend>Your datas</legend>
+
+          <Input name="name" label="Full name" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Bio" />
+        </fieldset>
+
+        <fieldset>
+          <legend>About the class</legend>
+
+          <Select
+            name="subject"
+            label="Subject"
+            options={[
+              { value: 'Art', label: 'Art' },
+              { value: 'Biology', label: 'Biology' },
+              { value: 'English', label: 'English' },
+              { value: 'Fisic', label: 'Fisic' },
+              { value: 'Geographic', label: 'Geographic' },
+              { value: 'History', label: 'History' },
+              { value: 'Mathematic', label: 'Mathematic' },
+              { value: 'Quimic', label: 'Quimic' },
+              { value: 'Science', label: 'Science' },
+            ]}
+          />
+          <Input name="cost" label="Cost of your class per day" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Free hours
+            <button type="button">
+              + New hour
+            </button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Week day"
+              options={[
+                { value: '0', label: 'Sunday' },
+                { value: '1', label: 'Monday' },
+                { value: '2', label: 'Tuesday' },
+                { value: '3', label: 'Wednesday' },
+                { value: '4', label: 'Thursday' },
+                { value: '5', label: 'Friday' },
+                { value: '6', label: 'Saturday' },
+              ]}
+            />
+            <Input type="time" name="from" label="From" />
+            <Input type="time" name="to" label="to" />
+          </div>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Important!
+            <br />
+            Fill in all the data
+          </p>
+          <button type="submit">Save datas</button>
+        </footer>
+      </Box>
+    </main>
+  </Container>
 );
 
 export default TeacherForm;
---
+++ packages/web/src/pages/TeacherForm/styles.ts
@@ -0,0 +1,147 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  main {
+    width: 100%;
+    padding: 0 20px;
+  }
+
+  @media (min-width: 700px) {
+    main {
+      max-width: 740px;
+      margin: 0 auto;
+      padding: 0;
+    } 
+  }
+`;
+
+export const Box = styled.div`
+  width: 100%;
+  margin-top: -32px;
+  padding-top: 20px;
+  border-radius: 8px;
+
+  background: var(--color-box-base);
+
+  fieldset {
+    border: 0;
+  }
+
+  fieldset + fieldset {
+    margin-top: 34px;
+  }
+
+  fieldset label {
+    color: var(--color-text-complement);
+  }
+
+  fieldset legend {
+    width: 100%;
+    padding-bottom: 16px;
+    border-bottom: 1px solid var(--color-line-in-white);
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    font: 700 24px Archivo;
+    color: var(--color-text-title);
+  }
+
+  fieldset legend button {
+    background: none;
+    border: 0;
+    color: var(--color-primary);
+    font: 700 16px Archivo;
+    cursor: pointer;
+    transition: color .2;
+  }
+
+  fieldset legend button:hover {
+    color: var(--color-primary-darker);}
+
+  footer {
+    margin-top: 64px;
+    padding: 40px 24px;
+    border-top: 1px solid var(--color-line-in-white);
+
+    background: var(--color-box-footer);
+  }
+
+  footer p {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 14px;
+    color: var(--color-text-complement);
+  }
+
+  footer p img {
+    margin-right: 2px;
+  }
+
+  footer button {
+    width: 100%;
+    height: 56px;
+    margin-top: 32px;
+    border: 0;
+    border-radius: 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    background: var(--color-secundary);
+    color: var(--color-button-text);
+    text-decoration: none;
+    transition: background-color .2s;
+  }
+
+  footer button:hover {
+    background: var(--color-secundary-dark);
+  }
+
+  @media (min-width: 700px) {
+    padding-top: 64px;
+
+    legend {
+      margin-bottom: 16px;
+    }
+
+    fieldset {
+      padding: 0 64px;
+    }
+
+    fieldset .schedule-item {
+      display: grid;
+      grid-template-columns: 2fr 1fr 1fr;
+      column-gap: 16px;
+    }
+
+    fieldset .schedule-item div {
+      margin-top: 0 !important;
+    }
+
+    footer {
+      padding: 40px 64px;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    footer p {
+      justify-content: space-between;
+    }
+
+    footer button {
+      width: 200px;
+      margin-top: 0;
+    }
+  }
+`;
