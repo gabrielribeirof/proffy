2839caf6b48490a6204af1678ada2b8cd8526d9a
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-06 19:39:01 -0300

    feat(web/TeacherList): create no teachers element

--- packages/web/src/pages/TeacherList/index.tsx
+++ packages/web/src/pages/TeacherList/index.tsx
@@ -1,79 +1,98 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import api from '@proffy/axios-config';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import { Container, SearchTeachers, NoTeachers } from './styles';
 
-import {
-  Container, SearchTeachers, TeacherItem, WhatsappButton,
-} from './styles';
+const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState([]);
 
-const TeacherList: React.FC = () => (
-  <Container>
-    <PageHeader title="These are the available proffys">
-      <SearchTeachers>
-        <Select
-          name="subject"
-          label="Subject"
-          options={[
-            { value: 'Art', label: 'Art' },
-            { value: 'Biology', label: 'Biology' },
-            { value: 'English', label: 'English' },
-            { value: 'Fisic', label: 'Fisic' },
-            { value: 'Geographic', label: 'Geographic' },
-            { value: 'History', label: 'History' },
-            { value: 'Mathematic', label: 'Mathematic' },
-            { value: 'Quimic', label: 'Quimic' },
-            { value: 'Science', label: 'Science' },
-          ]}
-        />
-        <Select
-          name="week_day"
-          label="Week day"
-          options={[
-            { value: '0', label: 'Sunday' },
-            { value: '1', label: 'Monday' },
-            { value: '2', label: 'Tuesday' },
-            { value: '3', label: 'Wednesday' },
-            { value: '4', label: 'Thursday' },
-            { value: '5', label: 'Friday' },
-            { value: '6', label: 'Saturday' },
-          ]}
-        />
-        <Input type="time" name="time" label="Hour" />
-      </SearchTeachers>
-    </PageHeader>
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
 
-    <main>
-      <TeacherItem>
-        <header>
-          <img src="https://avatars0.githubusercontent.com/u/39045611?s=460&u=443cbbc329d3206c1f34d11dff381b31cbb2ca83&v=4" alt="Teacher" />
-          <div>
-            <b>Gabriel Ribeiro</b>
-            <span>Mathematic</span>
-          </div>
-        </header>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          <br />
-          Praesent mollis erat arcu, vel blandit risus efficitur at.
-          Curabitur sagittis metus ac velit auctor.
-        </p>
-        <footer>
-          <p>
-            Price/hour
-            <b>$ 80,00</b>
-          </p>
-          <WhatsappButton>
-            <img src={whatsappIcon} alt="Whatsapp" />
-            Contact
-          </WhatsappButton>
-        </footer>
-      </TeacherItem>
-    </main>
-  </Container>
-);
+  async function searchTeachers(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      const response = await api.get('classes', {
+        params: {
+          subject,
+          week_day,
+          time,
+        },
+      });
+
+      setTeachers(response.data);
+    } catch {
+      setTeachers([]);
+    }
+  }
+
+  return (
+    <Container>
+      <PageHeader title="These are the available proffys">
+        <SearchTeachers onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            options={[
+              { value: 'Art', label: 'Art' },
+              { value: 'Biology', label: 'Biology' },
+              { value: 'English', label: 'English' },
+              { value: 'Fisic', label: 'Fisic' },
+              { value: 'Geographic', label: 'Geographic' },
+              { value: 'History', label: 'History' },
+              { value: 'Mathematic', label: 'Mathematic' },
+              { value: 'Quimic', label: 'Quimic' },
+              { value: 'Science', label: 'Science' },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Week day"
+            value={week_day}
+            onChange={(e) => setWeekDay(e.target.value)}
+            options={[
+              { value: '0', label: 'Sunday' },
+              { value: '1', label: 'Monday' },
+              { value: '2', label: 'Tuesday' },
+              { value: '3', label: 'Wednesday' },
+              { value: '4', label: 'Thursday' },
+              { value: '5', label: 'Friday' },
+              { value: '6', label: 'Saturday' },
+            ]}
+          />
+          <Input
+            type="time"
+            name="time"
+            label="Hour"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </SearchTeachers>
+      </PageHeader>
+
+      <main>
+        {teachers.length > 0 ? (
+          teachers.map((teacher: Teacher) => (
+            <TeacherItem key={teacher.id} teacher={teacher} />
+          ))
+        ) : (
+          <NoTeachers>
+            No teachers found with your search.
+          </NoTeachers>
+        )}
+      </main>
+    </Container>
+  );
+};
 
 export default TeacherList;
--- packages/web/src/pages/TeacherList/styles.ts
+++ packages/web/src/pages/TeacherList/styles.ts
@@ -11,24 +11,48 @@ export const Container = styled.div`
   @media (min-width: 700px) {
     main {
       padding: 32px 0;
-      max-width: 740px;
+      min-width: 740px;
       margin: 0 auto;
     } 
   }
 `;
 
 export const SearchTeachers = styled.form`
+  width: 100%;
   margin-top: 20px;
 
   label {
     color: var(--color-text-in-primary);
   }
 
+  button {
+    width: 100%;
+    height: 56px;
+    margin-top: 25px;
+    border: 0;
+    border-radius: 8px;
+    outline: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    background: var(--color-secundary);
+    color: var(--color-button-text);
+    text-decoration: none;
+    transition: background-color .2s;
+  }
+
+  button:hover {
+    background: var(--color-secundary-dark);
+  }
+
   @media (min-width: 700px) {
     margin-top: 0;
 
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     column-gap: 16px;
 
     position: absolute;
@@ -40,82 +64,11 @@ export const SearchTeachers = styled.form`
   }
 `;
 
-export const TeacherItem = styled.article`
-  margin-top: 24px;
-  border: 1px solid var(--color-line-in-white);
-  border-radius: 8px;
-
-  background: var(--color-box-base);
-  overflow: hidden;
-
-  header {
-    padding: 15px 20px;
-    display: flex;
-    align-items: center;
-  }
-
-  header img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-  }
-
-  header div {
-    margin-left: 24px;
-  }
-
-  header div b {
-    font: 700 24px Archivo;
-    display: block;
-    color: var(--color-text-title);
-  }
-
-  header div span {
-    font-size: 16px;
-    display: block;
-    margin-top: 4px;
-  }
-
-  > p {
-    padding: 0 20px;
-  }
-
-  footer {
-    padding: 0 20px;
-    margin-top: 32px;
-    border-top: 1px solid var(--color-line-in-white);
-
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    
-    background: var(--color-box-footer);
-  }
-
-  footer p b {
-    color: var(--color-primary);
-    font-size: 16px;
-    display: block;
-  }
-`;
-
-export const WhatsappButton = styled.button`
-  width: 200px;
-  height: 56px;
-  border: 0;
-  border-radius: 8px;
+export const NoTeachers = styled.span`
+  width: 100%;
+  height: 200px;
 
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-  
-  background: var(--color-secundary);
-  color: var(--color-button-text);
-  cursor: pointer;
-  font: 700 14px Archivo;
-  transition: .2s;
-
-  :hover {
-    background: var(--color-secundary-dark);
-  }
+  justify-content: center;
 `;
