d15a2758983bdbe6bfeb3f193138e56d1c0fddc7
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-08 19:38:51 -0300

    chore(web/webpack): implement react-dev-utils

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -1,8 +1,52 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const getPublicUrlOrPath = require('react-dev-utils/getPublicUrlOrPath');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const REACT_APP = /^REACT_APP_/i;
+
+function getClientEnvironment(publicUrl) {
+  const raw = Object.keys(process.env)
+    .filter(key => REACT_APP.test(key))
+    .reduce(
+      (env, key) => {
+        env[key] = process.env[key];
+        return env;
+      },
+      {
+        NODE_ENV: process.env.NODE_ENV || 'development',
+        PUBLIC_URL: publicUrl,
+        WDS_SOCKET_HOST: process.env.WDS_SOCKET_HOST,
+        WDS_SOCKET_PATH: process.env.WDS_SOCKET_PATH,
+        WDS_SOCKET_PORT: process.env.WDS_SOCKET_PORT,
+      }
+    );
+  const stringified = {
+    'process.env': Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {}),
+  };
+
+  return { raw, stringified };
+}
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const publicUrlOrPath = getPublicUrlOrPath(
+  process.env.NODE_ENV === 'development',
+  require(resolveApp('package.json')).homepage,
+  process.env.PUBLIC_URL
+);
+
+
+const env = getClientEnvironment(publicUrlOrPath.slice(0, -1));
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
@@ -14,23 +58,18 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|ts|tsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        loader: 'babel-loader'
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets',
-            },
-          },
-        ],
-      },
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets/images',
+          name: '[path][contenthash].[ext]'
+        }
+      }
     ],
   },
   devServer: {
@@ -42,13 +81,18 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/js/[hash].js',
-
+    filename: 'assets/js/[hash].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/assets', to: 'assets' },
+      ]
+    }),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
     isDevelopment && new ReactRefreshWebpackPlugin()
-  ].filter(Boolean),
+  ].filter(Boolean)
 };
