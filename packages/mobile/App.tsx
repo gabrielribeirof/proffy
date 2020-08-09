88090813a87270b5fbad885d74145dbf55eac554
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-09 02:11:31 -0300

    feat(mobile/App): load expo-font with AppLoading

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,15 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 import Landing from './src/pages/Landing';
 
-function App() {
+const App: React.FC = () => {
+  // eslint-disable-next-line prefer-const
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <Landing />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </>
   );
-}
+};
 
 export default App;
