3b4f62d3b563f96fec37e6f89e1b5fa04c7282ca
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-12 23:26:57 -0300

    feat(server/migrations): create user credentials

--- packages/server/src/database/migrations/00_create_users.ts
+++ packages/server/src/database/migrations/00_create_users.ts
@@ -4,9 +4,11 @@ export async function up(knex: Knex): Promise<Knex.SchemaBuilder> {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
     table.string('name').notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
     table.string('avatar').notNullable();
-    table.string('whatsapp').notNullable();
-    table.string('bio').notNullable();
+    table.string('whatsapp');
+    table.string('bio');
   });
 }
 
