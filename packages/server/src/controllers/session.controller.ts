7b58ba163fed75dac9fe472a771c50aa8c62404f
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-12 23:29:14 -0300

    feat(server/controllers): create session controlle

---
+++ packages/server/src/controllers/session.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+
+import db from '../database/connection';
+
+import { generateToken } from '../utils/json-web-token.util';
+
+export default {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { authorization } = request.headers;
+
+    try {
+      if (!authorization) {
+        return response.status(401).json({
+          error: 'Credentials not provided',
+        });
+      }
+
+      const parts = authorization.split(' ');
+      const [scheme, hash] = parts;
+
+      if (parts.length !== 2) {
+        return response.status(401).json({
+          error: 'Credentials error',
+        });
+      }
+
+      if (!/^Basic$/i.test(scheme)) {
+        return response.status(401).json({
+          error: 'Credentials malformatted',
+        });
+      }
+
+      const [email, password] = Buffer.from(hash, 'base64').toString().split(':');
+
+      const user = await db('users').where('email', email).select('id', 'password');
+
+      if (!user[0]) {
+        return response.status(401).json({
+          error: 'User not found',
+        });
+      }
+
+      if (!await bcrypt.compare(password, user[0].password)) {
+        return response.status(401).json({
+          error: 'Invalid password',
+        });
+      }
+
+      user[0].password = undefined;
+
+      return response.json({
+        token: generateToken({
+          data: {
+            id: user[0].id,
+          },
+        }),
+      });
+    } catch (err) {
+      return response.status(400).json({
+        error: 'Unexpected error when trying to login',
+      });
+    }
+  },
+};
