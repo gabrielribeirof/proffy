9d466c3673ef664840a9ecba2f64b088a07ea7ba
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-14 09:55:42 -0300

    feat(server/middlewares): create auth middleware

---
+++ packages/server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../config/auth.config';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: number;
+}
+
+export default (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Response | void => {
+  const { authorization } = request.headers;
+
+  try {
+    if (!authorization) {
+      return response.status(401).json({
+        error: 'Token not provided',
+      });
+    }
+
+    const parts = authorization.split(' ');
+    const [scheme, token] = parts;
+
+    if (parts.length !== 2) {
+      return response.status(401).json({
+        error: 'Token error',
+      });
+    }
+
+    if (!/^Bearer$/i.test(scheme)) {
+      return response.status(401).json({
+        error: 'Token malformatted',
+      });
+    }
+
+    const decodedPayload = jwt.verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedPayload as ITokenPayload;
+
+    request.payload = {
+      id: sub,
+    };
+
+    return next();
+  } catch (err) {
+    return response.status(400).json({
+      error: 'Unexpected error in authentication',
+    });
+  }
+};
