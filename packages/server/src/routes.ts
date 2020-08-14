9cbb34be7b7bab9442734e0c60fffce99456a05b
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-08-14 09:57:23 -0300

    feat(server/routes): use new controllers

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 
+import SessionController from './controllers/session.controller';
+import UsersController from './controllers/users.controller';
 import ClassesController from './controllers/classes.controller';
 import ConnectionsController from './controllers/connections.controller';
 
+import AuthMiddleware from './middlewares/auth.middleware';
+
 const routes = express.Router();
 
+routes.post('/login', SessionController.create);
+
+routes.post('/users', AuthMiddleware, UsersController.create);
+
 routes.get('/classes', ClassesController.index);
-routes.post('/classes', ClassesController.create);
+routes.post('/classes', AuthMiddleware, ClassesController.create);
 
 routes.get('/connections', ConnectionsController.index);
 routes.post('/connections', ConnectionsController.create);
